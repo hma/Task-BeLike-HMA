2bd4f406c9bee016bd77a999f2bec1d4a9197624
Henning Manske <hma@cpan.org>
2010-10-10 16:16:39 +0200

    Bumped version to 0.03

--- lib/Task/BeLike/HMA.pm
+++ lib/Task/BeLike/HMA.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use 5.006001;
 
-our $VERSION = '0.02_01';
+our $VERSION = '0.03';
 
 $VERSION = eval $VERSION;
 
@@ -19,7 +19,7 @@ Task::BeLike::HMA - Install modules HMA uses
 
 =head1 VERSION
 
-This is version 0.02_01 of Task::BeLike::HMA as of Sep 19, 2010.
+This is version 0.03 of Task::BeLike::HMA as of Oct 10, 2010.
 
 =head1 SYNOPSIS
 
